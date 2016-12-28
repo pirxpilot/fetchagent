0e324f633c26d69f7d9f795c05ea82a10c9950af
Damian Krzeminski <pirxpilot@code42day.com>
2016-12-27 21:16:24 -0800

    add tests

--- test/fetchagent.js
+++ test/fetchagent.js
@@ -1,12 +1,61 @@
 var should = require('should');
 var fetchagent = require('../');
 
+var Replay = require('replay');
+
+Replay.fixtures = __dirname + '/fixtures';
+
 describe('fetchagent', function () {
 
-  it('should get some text', function() {
+  it('should get some text', function(done) {
+    fetchagent
+      .get('http://httpbin.org/xml')
+      .end(function(err, response) {
+        should.not.exist(err);
+        response.should.startWith('<?xml');
+        done();
+      });
   });
 
   it('should post and receive json', function() {
+    return fetchagent
+      .post('http://httpbin.org/post')
+      .send({ echo: 42 })
+      .json()
+      .should.finally.have
+        .property('json')
+        .property('echo', 42);
   });
 
+  it('should format query parameters', function() {
+    return fetchagent
+      .get('http://httpbin.org/get')
+      .query({ echo: 42, ll: [14, 30], name: 'with space?' })
+      .json()
+      .should.finally.have
+        .property('args', {
+          echo: '42',
+          ll: '14,30',
+          name: 'with space?'
+        });
+  });
+
+  it('should set request headers', function() {
+    return fetchagent
+      .get('http://httpbin.org/get')
+      .set('x-my-header', 'bongo')
+      .json()
+      .should.finally.have
+        .property('headers')
+        .property('X-My-Header', 'bongo');
+  });
+
+  it('should automatically redirect', function () {
+    return fetchagent
+      .get('http://httpbin.org/redirect/2')
+      .redirect(true)
+      .json()
+      .should.finally.have
+        .property('url');
+  });
 });
